401c680239a80fed73c601a51a8eaf703bd86088
daniellutziger <daniel.lutziger@uzh.ch>
2023-02-23 11:11:19 +0100

    added new attributes to user and the corresponding user dto

--- src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/UserPostDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/UserPostDTO.java
@@ -1,24 +1,44 @@
 package ch.uzh.ifi.hase.soprafs23.rest.dto;
 
+import java.util.Date;
+
 public class UserPostDTO {
 
-  private String name;
+    private String name;
 
-  private String username;
+    private String username;
+    private String password;
+    private Date birthday;
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public String getUsername() {
-    return username;
-  }
+    public String getUsername() {
+        return username;
+    }
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
 }
