ae1934e641204e0d6fbaaaf1ecfd1ab58ce4f40b
daniellutziger <daniel.lutziger@uzh.ch>
2023-03-15 15:16:18 +0100

    asdffff

--- src/main/java/ch/uzh/ifi/hase/soprafs23/security/WebSecurityConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/security/WebSecurityConfig.java
@@ -1,71 +1,35 @@
-package ch.uzh.ifi.hase.soprafs23.security;
+@Bean
+public AuthenticationManager authenticationManagerBean() throws Exception {
+        return authenticationManager();
+        }
 
-import ch.uzh.ifi.hase.soprafs23.security.jtw.JwtAuthenticationEntryPoint;
-import ch.uzh.ifi.hase.soprafs23.security.jtw.JwtAuthorizationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerBuilder;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+@Bean
+public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+        }
 
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig implements WebSecurityConfigurerBuilder {
-
-    @Autowired
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-    @Autowired
-    private UserDetailsService jwtUserDetailsService;
-
-    @Autowired
-    private JwtAuthorizationFilter jwtAuthorizationFilter;
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http    // enable cors and disable csrf
-                .cors().and().csrf().disable()
-                // Endpoints which should not be targeted by the restrictions
-                .authorizeRequests().mvcMatchers("/login").permitAll()
-                .mvcMatchers(HttpMethod.GET, "/users").authenticated()
-                .mvcMatchers(HttpMethod.POST, "/users").permitAll()
-                // All other requests need to be authenticated
-                .anyRequest().authenticated().and()
-                // Make sure we use stateless session; session won't be used to store user's state.
-                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        // Add a filter to validate the tokens with every request
-        http.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().mvcMatchers("/h2-console/**");
-    }
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
+@Bean
+public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-}
+        }
+
+@Bean
+public UserDetailsService userDetailsService() {
+        return new MyUserDetailsService();
+        }
+
+@Bean
+public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+        .authorizeExchange()
+        .pathMatchers("/public/**").permitAll()
+        .anyExchange().authenticated()
+        .and()
+        .httpBasic()
+        .and()
+        .csrf().disable()
+        .build();
+        }
